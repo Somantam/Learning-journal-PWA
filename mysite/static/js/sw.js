5eef5abca0bfbc813bef9621de218520bfc719ef
Somantam <tamangsoman521@gmail.com>
2025-12-11 13:01:55 +0000

    Create sw.js lab 7

---
+++ mysite/static/js/sw.js
@@ -0,0 +1,53 @@
+// mysite/static/sw.js
+
+const CACHE_NAME = 'journal-v1';
+const ASSETS_TO_CACHE = [
+    '/',                  // Homepage
+    '/journal',           // Journal Page
+    '/projects',          // Projects Page
+    '/about',             // About Page
+    '/static/css/style.css',
+    '/static/js/storage.js',
+    '/static/js/script.js',
+    '/static/js/browser.js',
+    '/static/js/jsonhandler.js',
+    '/static/js/thirdparty.js',
+    '/static/manifest.json',
+    '/static/images/icon-192.png', // Ensure these exist!
+    '/static/images/icon-512.png'
+];
+
+// 1. Install Event: Cache static assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('[Service Worker] Caching all assets');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// 2. Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[Service Worker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// 3. Fetch Event: Network First, fallback to Cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
